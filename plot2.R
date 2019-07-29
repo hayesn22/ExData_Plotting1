2b7e9d87c6f9b24dbf8084ee2b9b8d93a596a956
hayesn22 <hayesn22@wclive.westminster.edu>
2019-07-28 20:15:19 -0400

    Uploading project

---
+++ plot2.R
@@ -0,0 +1,11 @@
+householdpower <- read.table("household_power_consumption.txt",skip=1,sep=";", na.strings = "?", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+names(householdpower) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subhousepower <- subset(householdpower,householdpower$Date=="1/2/2007" | householdpower$Date =="2/2/2007")
+subhousepower$Date <- as.Date(subhousepower$Date, format="%d/%m/%Y")
+subhousepower$Time <- strptime(subhousepower$Time, format="%H:%M:%S")
+subhousepower[1:1440,"Time"] <- format(subhousepower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subhousepower[1441:2880,"Time"] <- format(subhousepower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+plot(subhousepower$Time,subhousepower$Global_active_power, type = "l",xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.copy(png, "plot2.png", width = 480, height = 480)
+dev.off()
